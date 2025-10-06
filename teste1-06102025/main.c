09891b50a851e43ed7335ae72b538f4e7eda0f47
dieguscl <diegocastrolaya@gmail.com>
2025-10-06 17:54:45 +0100

    initial commit

---
+++ teste1-06102025/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+    printf("Hello world!\n");
+    printf("Hola, cómo estás!\n");
+    printf("Olá!\n");
+
+    return 0;
+}
